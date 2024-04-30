0282eaa46873fe4c1f2aefaa2ab5d488b118500b
Caio Henrique <caiohenrqq@gmail.com>
2024-04-29 20:10:44 -0400

    Adiciona API estados e coloca em select no HTML

--- js/script.js
+++ js/script.js
@@ -15,3 +15,35 @@ window.onbeforeunload = function() {
 document.addEventListener('DOMContentLoaded', function() {
   AOS.init();
 });
+
+const urlEstados = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+var selectEstados = document.getElementById('campoEstados');
+var option;
+
+fetch(urlEstados)
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error ('Erro ao obter os dados da API dos estados.')
+    }
+
+    return res.json();
+  })
+    .then((data => {
+      let i = 0;
+      data.forEach((estado) => {
+        option = document.createElement('option');
+        option.textContent = estado.nome;
+        option.setAttribute('data-sigla', estado.sigla);
+        selectEstados.appendChild(option);
+
+        // Logs
+        console.log(estado.nome + ',', estado.sigla + '.')
+        
+        i++
+      });
+      console.log(i + ' estados encontrados.')
+    }))
+    .catch((error) => {
+      console.error('Ocorreu um erro ao buscar os dados:', error);
+    })
